3348cee84613774f0da9b2dbaec9cac702cdfe85
Jan Papousek <jan.papousek@gmail.com>
2016-03-31 08:44:23 +0200

    ratings per success with confidence intervals

--- commands/ab/ratings.py
+++ commands/ab/ratings.py
@@ -1,6 +1,7 @@
 from .raw import load_success, load_school_usage, load_ratings
 from pylab import rcParams
 from spiderpig import spiderpig
+from metric import binomial_confidence_mean
 import matplotlib.pyplot as plt
 import output
 import pandas
@@ -19,49 +20,62 @@ def ratings_per_success(user_limit=50, in_school=None):
 
     success = load_success(first=30, round_base=5)
     result = []
+
     for group_success, group_data in success.reset_index().groupby(0):
         group_ratings = ratings[ratings['user_id'].isin(group_data['user_id'])]
         if len(group_ratings) < user_limit:
             continue
-        easy = group_ratings[group_ratings['value'] == 1]
-        appropriate = group_ratings[group_ratings['value'] == 2]
-        difficult = group_ratings[group_ratings['value'] == 3]
+        easy = binomial_confidence_mean(group_ratings['value'] == 1)
+        appropriate = binomial_confidence_mean(group_ratings['value'] == 2)
+        difficult = binomial_confidence_mean(group_ratings['value'] == 3)
         result.append({
             'success': group_success,
-            'easy': len(easy) / float(len(group_ratings)),
-            'appropriate': len(appropriate) / float(len(group_ratings)),
-            'difficult': len(difficult) / float(len(group_ratings)),
+            'easy_value': easy[0],
+            'easy_confidence_min': easy[1][0],
+            'easy_confidence_max': easy[1][1],
+            'appropriate_value': appropriate[0],
+            'appropriate_confidence_min': appropriate[1][0],
+            'appropriate_confidence_max': appropriate[1][1],
+            'difficult_value': difficult[0],
+            'difficult_confidence_min': difficult[1][0],
+            'difficult_confidence_max': difficult[1][1],
             'total': len(group_ratings),
         })
 
     return pandas.DataFrame(result)
 
 
-def plot_ratings_per_success(user_limit, in_school=None, legend=True):
+def plot_ratings_per_success(user_limit, in_school=None, legend=True, with_confidence=True):
     data = ratings_per_success(user_limit=user_limit, in_school=in_school)
-    plt.plot(data['success'], data['easy'], label='too easy')
-    plt.plot(data['success'], data['appropriate'], label='appropriate')
-    plt.plot(data['success'], data['difficult'], label='too difficult')
+    for i, (r_type, marker) in enumerate(zip(['easy', 'appropriate', 'difficult'], ['o', 's', '^'])):
+        plt.plot(data['success'], data['{}_value'.format(r_type)], label=r_type, color=output.palette()[i], marker=marker)
+        if with_confidence:
+            plt.fill_between(
+                data['success'],
+                data['{}_confidence_min'.format(r_type)],
+                data['{}_confidence_max'.format(r_type)],
+                color=output.palette()[i], alpha=0.35
+            )
     if legend:
         plt.legend(loc=1)
     plt.xlabel('Success rate')
     plt.ylim(0, 1)
 
 
-def plot_ratings(user_limit):
-    plot_ratings_per_success(user_limit)
-    output.savefig(filename='ratings_per_success')
+def plot_ratings(user_limit, with_confidence):
+    plot_ratings_per_success(user_limit, with_confidence=with_confidence)
+    output.savefig('ratings_per_success')
     rcParams['figure.figsize'] = 15, 5
     plt.subplot(121)
     plt.title('in-school users')
-    plot_ratings_per_success(user_limit, in_school=True)
+    plot_ratings_per_success(user_limit, in_school=True, with_confidence=with_confidence)
     plt.xlim(0.5, 1)
     plt.subplot(122)
     plt.title('out-of-school users')
-    plot_ratings_per_success(user_limit, in_school=False)
+    plot_ratings_per_success(user_limit, in_school=False, with_confidence=with_confidence, legend=False)
     plt.xlim(0.5, 1)
-    output.savefig(filename='ratings_per_success_school_usage')
+    output.savefig('ratings_per_success_school_usage')
 
 
-def execute(user_limit=50):
-    plot_ratings(user_limit)
+def execute(user_limit=50, with_confidence=True):
+    plot_ratings(user_limit, with_confidence)
